6ec0a16dbd4842d5c8e8947d7a91522b5f231bc3
tbansod2018 <tbansod2020@gmail.com>
2024-07-13 06:08:46 +0530

    updated

--- Frontend/src/components/About.js
+++ Frontend/src/components/About.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import "./About.css"; // Make sure the path to About.css is correct
-import Accordion from "./Accordion/Accordion";
-import { accordionData } from "./Accordion/AccordionData";
 
 const About = () => {
   // Destructure useState properly and rename variables if needed
